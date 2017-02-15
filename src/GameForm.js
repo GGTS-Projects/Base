e8b779a0e9b6941d15922791df0e6495fa0a8e59
umar.abbas <omer.ithj@gmail.com>
2017-02-16 01:33:26 +0500

    Gameform

--- src/GameForm.js
+++ src/GameForm.js
@@ -1,12 +1,45 @@
 import React from 'react';
 
-class GameForm extends React.Component{
+class GameForm extends React.Component {
+    state = {
+        title: '',
+        cover: ''
+    }
 
-    render(){
-        return(
-            <div>
-             <h1>GameForm</h1>
-            </div>
+    handelChange =(e)=>{
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    render() {
+        return (
+            <form className="ui form">
+                <h1>Add New Games</h1>
+                <div className="field">
+                    <label htmlFor="title">Title</label>
+                    <input
+                        name="title"
+                        value={this.state.title}
+                        onChange={this.handelChange}
+                        id="title"/>
+                </div>
+
+                <div className="field">
+                    <label htmlFor="cover">Cover</label>
+                    <input
+                        name="cover"
+                        value={this.state.cover}
+                        onChange={this.handelChange}
+                        id="cover"/>
+                </div>
+                <div className="field">
+                {this.state.cover!==''  &&  <img src={this.state.cover} alt="cover" className="ui small bordered"/>}
+
+                </div>
+                <div className="field">
+                    <button className="ui primary button">Save</button>
+
+                </div>
+            </form>
         );
     }
 }
