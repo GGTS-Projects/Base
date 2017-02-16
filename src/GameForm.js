e9b6afd894503425b37d0fbf87efa2a11ee8a4d9
umar.abbas <omer.ithj@gmail.com>
2017-02-16 19:24:03 +0500

    End 4 part

--- src/GameForm.js
+++ src/GameForm.js
@@ -1,38 +1,68 @@
 import React from 'react';
+import classnames from 'classnames';
 
 class GameForm extends React.Component {
     state = {
         title: '',
-        cover: ''
+        cover: '',
+        errors: {}
     }
 
-    handelChange =(e)=>{
-        this.setState({[e.target.name]: e.target.value});
-    }
+    handleChange = (e) => {
+        if(!!this.state.errors[e.target.name]){
 
+        let errors = Object.assign({}, this.state.errors);
+        delete errors[e.target.name];
+        this.setState({
+            [e.target.name]: e.target.value,
+            errors
+        });
+    } else{
+            this.setState({[e.target.name]:e.target.value})
+        }
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        //validation
+        let errors = {};
+        if (this.state.title === '') 
+            errors.title = "Can't be empty";
+        if (this.state.cover === '') 
+            errors.cover = "Can't be empty";
+        this.setState({errors});
+    }
     render() {
         return (
-            <form className="ui form">
+            <form className="ui form" onSubmit={this.handleSubmit}>
                 <h1>Add New Games</h1>
-                <div className="field">
+                <div
+                    className={classnames('field', {
+                    error: !!this.state.errors.title
+                })}>
                     <label htmlFor="title">Title</label>
                     <input
                         name="title"
                         value={this.state.title}
-                        onChange={this.handelChange}
+                        onChange={this.handleChange}
                         id="title"/>
+                    <span>{this.state.errors.title}</span>
                 </div>
 
-                <div className="field">
+                <div
+                    className={classnames('field', {
+                    error: !!this.state.errors.cover
+                })}>
                     <label htmlFor="cover">Cover</label>
                     <input
                         name="cover"
                         value={this.state.cover}
-                        onChange={this.handelChange}
+                        onChange={this.handleChange}
                         id="cover"/>
+                    <span>{this.state.errors.cover}</span>
                 </div>
                 <div className="field">
-                {this.state.cover!==''  &&  <img src={this.state.cover} alt="cover" className="ui small bordered image"/>}
+                    {this.state.cover !== '' && <img src={this.state.cover} alt="cover" className="ui small bordered image"/>}
 
                 </div>
                 <div className="field">
