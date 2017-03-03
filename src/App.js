d4ae020560c33272780e436fc1b01f4c3070b401
Umar Abbas <Umar Abbas>
2017-03-03 07:10:26 +0500

    Bootstrap Added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {Link, Match} from 'react-router';
 class App extends Component {
   render() {
     return (
-      <div className="ui container">
+      <div className="container">
 
         <div className="ui three item menu">
           <Link className="item" activeClassName="active" activeOnlyWhenExact to="/">Home</Link>
---
+++ src/components/Layout/header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link, IndexLink } from 'react-router';
+
+
+const Header=()=>(
+
+<div>
+<div className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div className="container">
+   
+            <div className="navbar-header">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="#">Start Bootstrap</a>
+            </div>
+          
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+         
+        </div>
+
+    </div>
+       
+</div>
+);
+
+export default Header;
+
+
